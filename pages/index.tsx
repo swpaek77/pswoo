b47d4e2514c1e6a8224ff7d4a14420154e4c8aa7
Seungwoo Paek <swpaek77@gmail.com>
2022-04-28 00:14:38 +0900

    필요없는 것 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,52 +13,8 @@ import { LoginInfoRedux } from '../redux/LoginRedux';
 import Animation from '../component/Animation';
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const [products, setProducts] = useState<any[]>([]);
-  const [contacts, setContacts] = useState<any[]>([]);
-  const loginInfo = useSelector(({ LoginRedux }: any) => LoginRedux.loginInfo);
-
-  useEffect(() => {
-    getLoginInfo();
-    getProducts();
-  }, []);
-
-  const getLoginInfo = async () => {
-    const userData = localStorage.userData;
-
-    if (userData) {
-      dispatch(LoginInfoRedux(JSON.parse(userData)));
-    }
-  };
-
-  const getProducts = async () => {
-    try {
-      const { data } = await axios.get('https://api.pswoo.com/products');
-      setProducts(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   const preventClick = (e: any) => {
     e.preventDefault();
-  };
-
-  const getAuthContacts = async () => {
-    try {
-      const { data } = await axios.get('https://api.pswoo.com/contacts', {
-        params: {
-          _sort: 'created_at:DESC',
-        },
-        headers: {
-          authorization: `Bearer ${JSON.parse(localStorage.userData).jwt}`,
-        },
-      });
-      setContacts(data);
-    } catch (err) {
-      console.log(err);
-      alert(translate.ko[StrapiError(err)] || '치명적인 오류');
-    }
   };
 
   return (
